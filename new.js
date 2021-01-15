d5002d55a7248f019877d7ce8f466062c6ca6dfa
Boris <a402539@yandex.ru>
2021-01-15 23:09:00 +0300

    AskBid - timeStamp

--- new.js
+++ new.js
@@ -24,16 +24,18 @@ function act(){
 	wasBid = Bid;
 };
 var AskBid = []; // массив Ask, Bid стакана
+var timeStamp = 0;
 function getAskBid(){
+	timeStamp = timeStamp + 10;
 	var lastAsk = document.getElementsByClassName("trade-management__main-items")[0].children;
 	var Ask = Number(lastAsk[lastAsk.length-3].getAttribute("data-price"));
 	var lastBid = document.getElementsByClassName("marked-list trade-sell-orders")[0].children[0].children[0];
 	var Bid = Number(lastBid.getAttribute("data-price"));
-	console.log("на входе", Ask, Bid);
+	//console.log("на входе", Ask, Bid);
 	var nAskBid = AskBid.length;
-	if(!nAskBid || AskBid[nAskBid-1][0] != Ask || AskBid[nAskBid-1][1] != Bid)	{
-		AskBid.push([Ask, Bid]);
-		console.log(AskBid.length, Ask, Bid);
+	if(!nAskBid || AskBid[nAskBid-1][1] != Ask || AskBid[nAskBid-1][2] != Bid)	{
+		AskBid.push([timeStamp, Ask, Bid]);
+		console.log(AskBid.length, timeStamp, Ask, Bid);
 	};
 };
 setInterval(getAskBid, 1000*10); // проверяем с интервалом в 10 секунд (записываем только если есть изменения в Ask или Bid)
